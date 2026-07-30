fe5c1b9682a2f015b5da7ec910ab75e980c449ac
chensong <chensong@meituan.com>
2026-07-30 17:43:08 +0800

    style: move portfolio footer toggles to right side for balanced layout

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -197,7 +197,7 @@ export default function Portfolio() {
         showGallerySubtitle={false}
         backHref="/"
         maskHeight="240px"
-        togglesSide="left"
+        togglesSide="right"
         hideRight="mobile"
       />
 
